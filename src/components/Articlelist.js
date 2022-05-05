a12b30e10b5c906a1f3869cecd3c238df609c96f
Irene Njeru <irenenjeru8@gmail.com>
2022-05-05 22:00:26 +0300

    task completed

---
+++ src/components/Articlelist.js
@@ -0,0 +1,18 @@
+import Article from "./Article";
+
+function ArticleList(props){
+  
+    const articles = props.posts.map(post => (
+      <Article
+        key = {post.id}
+        date = {post.date}
+        preview = {post.preview}
+        title = {post.title}
+      />
+      
+    ));
+    
+    return <main>{articles}</main>
+}
+
+export default ArticleList;
